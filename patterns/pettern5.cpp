029a63398e7d02bffc05d2fd62b07dfcc00be9a4
vidhijindal03 <jindalvartika9@gmail.com>
2025-09-04 23:37:46 +0530

    c++ basics

---
+++ patterns/pettern5.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+void pattern(){
+    for(int i=0;i<5;i++){
+        for(int j=0;J<5;j++){
+            cout<<i;
+
+        }
+    }
+    cout<<endl;
+}
+int main(){
+    pattern()
+    return 0;
+}
