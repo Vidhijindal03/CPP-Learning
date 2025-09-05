3a2d95a57c8fc52d10e3242a9e005cf98609f6e4
vidhijindal03 <jindalvartika9@gmail.com>
2025-09-05 18:04:12 +0530

    c++ baisc

---
+++ patterns/pattern11.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+void pattern(int n){
+    for(int i=0;i<n;i++){
+        if(i%2==0){
+            for(int j=0;j<=i;j++){
+                if(j%2==0){
+                    cout<<"1";
+                }
+                
+            
+            else{
+                cout<<"0";
+            }
+            }
+            cout<<endl;
+        }
+
+        else {
+            for(int j=0;j<=i;j++){
+                if(j%2==0){
+                    cout<<"0";
+                }
+                else{
+                    cout<<"1";
+                }
+            }
+
+            cout<<endl;
+
+        }
+    
+    }
+}
+int main (){
+    int n;
+    cin>>n;
+    pattern(n);
+    return 0;
+}
---
+++ patterns/pattern12.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+using namespace std;
+
+void pattern(int n){
+    int space = 2* (n-1);
+ for(int i=1;i<=n;i++){
+    for(int j=1;j<=i;j++){
+        cout<<j;
+    }  
+    for(int j=1;j<=space;j++){
+        cout<<" ";
+    } 
+    for(int j=i;j>=1;j--){
+        cout<<j;
+    } 
+    cout<<endl;
+    space -=2;
+ }   
+}
+int main(){
+    int n;
+    cin>>n;
+    pattern(n);
+    return 0;
+}
+    /*
+void print1(int n){
+    int space = 2* (n-1);
+    for(int i=1;i<=n;i++){
+        //number
+        for(int j=1;j<=i;j++){
+            cout<<j;
+        }
+        //space
+        for(int j=1;j<=space;j++){
+            cout<<" ";
+        }
+        //number
+        for(int j=i;j>=1;j--){
+            cout<< j;
+        }
+        cout<< endl;
+        space -=2;
+    }
+}
+int main(){
+    int n;
+    cin>>n;
+    print1(n);
+    return 0;
+}
+    */
