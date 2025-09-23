88e3e4baee0b0ee603c0baa4ffde0d1d854d53a4
vidhijindal03 <jindalvartika9@gmail.com>
2025-09-23 19:05:11 +0530

    Basic Maths

---
+++ Basic maths/count.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int count(int n){
+    int cnt=(int)(log10(n)+1);
+    return cnt;
+}
+
+int main(){
+    int n;
+    cin>>n;
+    cout<<"number of digits"<<count(n)<<endl;
+    return n;
+}
